4b33349c535620d463b0ae939b8551bbca7b277b
Xu-pixel <naomi1299@163.com>
2026-04-03 18:53:00 +0800

    feat: Enhance chat completion endpoint with authorization and model validation; update message handling

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,30 @@ async function* chatSSE(messages: any[]) {
 }
 
 new Elysia()
-  .post('/v1/chat/completions', ({ body }) => {
+  .post('/v1/chat/completions', ({ body,headers }) => {
+    if(headers['authorization'] !== process.env.DOWNSTREAM_KEY) {
+      throw new Error('Unauthorized')
+    }
+
+    if(body.model !== process.env.DOWNSTREAM_MODEL_NAME) {
+      throw new Error('Model not supported')
+    }
+
     // 把body中的dialogue转成messages
-    const messages = body.dialogue.map((item: any) => ({
+    let messages = body.dialogue.map((item: any) => ({
       role: item.role,
       content: item.content,
     }))
+
+    //查找用户请求中的system prompt 如果有则删除,可能有多个
+    messages = messages.filter(item => item.role !== 'system')
+
+    // 添加我们的system prompt
+    messages.unshift({
+      role: 'system',
+      content: process.env.SYSTEM_PROMPT!,
+    })
+
     if (body.stream) {
       return sse(chatSSE(messages))
     } else {
@@ -38,6 +56,7 @@ new Elysia()
   }, {
     body: t.Object({
       stream: t.Optional(t.Boolean()),
+      model: t.String(),
       // 从openai的api文档中复制出来的
       dialogue: t.Array(t.Any()),
     }, { additionalProperties: true })
